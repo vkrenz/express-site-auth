7af468cec278d782d35f99d175da7e5c06fafe21
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2022-11-11 18:04:30 -0500

    Assignment 4 complete

--- routes/blog.js
+++ routes/blog.js
@@ -53,10 +53,11 @@ Blog.exists({blogID: 1}, (err, entry) => {
 })
 
 // new Blog({
-//     blogID: 2,
-//     name: "Blockchain Facts: What Is It, How It Works, and How It Can Be Used",
-//     content: "What Is a Blockchain?A blockchain is a distributed database or ledger that is shared among the nodes of a computer network. As a database, a blockchain stores information electronically in digital format. Blockchains are best known for their crucial role in cryptocurrency systems, such as Bitcoin, for maintaining a secure and decentralized record of transactions. The innovation with a blockchain is that it guarantees the fidelity and security of a record of data and generates trust without the need for a trusted third party.<br>One key difference between a typical database and a blockchain is how the data is structured. A blockchain collects information together in groups, known as blocks, that hold sets of information. Blocks have certain storage capacities and, when filled, are closed and linked to the previously filled block, forming a chain of data known as the blockchain. All new information that follows that freshly added block is compiled into a newly formed block that will then also be added to the chain once filled.<br>A database usually structures its data into tables, whereas a blockchain, as its name implies, structures its data into chunks (blocks) that are strung together. This data structure inherently makes an irreversible timeline of data when implemented in a decentralized nature. When a block is filled, it is set in stone and becomes a part of this timeline. Each block in the chain is given an exact timestamp when it is added to the chain.",
-//     blogEntryImgURL: "https://www.investopedia.com/thmb/wuuss_5lSKqGckNngtP1__7qEk4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Blockchain_final-086b5b7b9ef74ecf9f20fe627dba1e34.png"
+//     blogID: 3,
+//     name: "What Are Web 2.0 and Web 3.0?",
+//     category: "Web 3.0",
+//     content: "Web 2.0 and Web 3.0 refer to successive iterations of the web, compared with the original Web 1.0 of the 1990s and early 2000s. Web 2.0 is the current version of the internet (a term often used interchangeably with the web) with which we are all familiar. Web 3.0 or Web3 is the third generation of the World Wide Web. Currently a work in progress, it is a vision of a decentralized and open Web with greater utility for its users. Web refers to the World Wide Web (WWW), the internet’s core information retrieval system. The WWW initialism used to (and often still does) preface a web address and was one of the first characters typed into a web browser when searching for a specific resource online. Internet pioneer Tim Berners-Lee is credited with coining the term World Wide Web to refer to the global web of information and resources interconnected through hypertext links.",
+//     blogEntryImgURL: "https://www.trustedreviews.com/wp-content/uploads/sites/54/2021/11/Meta-image-920x518.png"
 // }).save()
 
 router.get('/', (req, res) =>{
